f918245f45806691dd8751f87000b1d4395bef55
tttol <tkhstol.929@gmail.com>
2026-04-01 06:44:38 +0900

    update

---
+++ src/plugins/remark-mermaid.mjs
@@ -0,0 +1,13 @@
+import { visit } from 'unist-util-visit';
+
+export function remarkMermaid() {
+    return (tree) => {
+        visit(tree, 'code', (node, index, parent) => {
+            if (node.lang !== 'mermaid') return;
+            parent.children.splice(index, 1, {
+                type: 'html',
+                value: `<div class="mermaid">${node.value}</div>`,
+            });
+        });
+    };
+}
