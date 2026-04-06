f3e86b7702fd6079879dc499cd54191a9622d36f
tttol <tkhstol.929@gmail.com>
2026-04-07 06:00:56 +0900

    Fix mermaid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,19 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import remarkBreaks from 'remark-breaks';
+import rehypeRaw from 'rehype-raw';
 import { defineConfig } from 'astro/config';
 import { remarkMermaid } from './src/plugins/remark-mermaid.mjs';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.about-tttol.link',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx({
+			rehypePlugins: [rehypeRaw],
+		}),
+		sitemap(),
+	],
 	markdown: {
 		remarkPlugins: [remarkBreaks, remarkMermaid],
 	},
