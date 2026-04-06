707e2f1dd525c8b922ded97dc023bb866fc80c75
Toru Takahashi <tkhstol.929@gmail.com>
2026-04-07 05:49:42 +0900

    ちゃんと理解したいシリーズ：証明書 (#14)
    * update
    
    * fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import remarkBreaks from 'remark-breaks';
 import { defineConfig } from 'astro/config';
+import { remarkMermaid } from './src/plugins/remark-mermaid.mjs';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.about-tttol.link',
 	integrations: [mdx(), sitemap()],
 	markdown: {
-		remarkPlugins: [remarkBreaks],
+		remarkPlugins: [remarkBreaks, remarkMermaid],
 	},
 });
